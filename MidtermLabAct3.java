45fe3cb2d45873efa3221e41e62ea308b49911ba
James Gabriel Turceno <128990947+jimsdakrinj@users.noreply.github.com>
2026-03-16 19:20:24 +0800

    Add files via upload

---
+++ MidtermLabAct3.java
@@ -0,0 +1,20 @@
+import java.util.*
+
+public class MidtermLabAct3 {
+   public static void main(String[] args){
+   Scanner scanf = new Scanner(System.in);
+   
+   String[] names = new String[3];
+   int gradeVar = new int[4];
+   
+   for(int i = 0; i < names.lenght; i++){
+      System.out.printf("Enter name of Student %d: ", i);
+      
+      }
+   
+   }
+   
+   public static int Grades(int grades) {
+      
+   }
+}
